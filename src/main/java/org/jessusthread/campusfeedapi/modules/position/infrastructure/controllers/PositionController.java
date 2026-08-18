f1b06bfc0cba315691dbfc91c949152ebe79f9d9
jessus.thread <jessus.thread@outlook.com>
2026-08-17 20:26:06 -0600

    feat: implement a route to delete a position by id

--- src/main/java/org/jessusthread/campusfeedapi/modules/position/infrastructure/controllers/PositionController.java
+++ src/main/java/org/jessusthread/campusfeedapi/modules/position/infrastructure/controllers/PositionController.java
@@ -2,25 +2,38 @@ package org.jessusthread.campusfeedapi.modules.position.infrastructure.controlle
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import lombok.AllArgsConstructor;
+import org.jessusthread.campusfeedapi.modules.position.application.usecases.CreatePositionUseCase;
+import org.jessusthread.campusfeedapi.modules.position.application.usecases.DeletePositionUseCase;
 import org.jessusthread.campusfeedapi.modules.position.application.usecases.GetPositionsUseCase;
+import org.jessusthread.campusfeedapi.modules.position.application.usecases.UpdatePositionUseCase;
 import org.jessusthread.campusfeedapi.modules.position.domain.Position;
 
 import java.util.List;
 
+@AllArgsConstructor
 public class PositionController {
     private final GetPositionsUseCase getPositionsUseCase;
-
-    public PositionController(GetPositionsUseCase getPositionsUseCase) {
-        this.getPositionsUseCase = getPositionsUseCase;
-    }
+    private final DeletePositionUseCase deletePositionUseCase;
+    private final CreatePositionUseCase createPositionUseCase;
+    private final UpdatePositionUseCase updatePositionUseCase;
 
     public void registerRoutes(Javalin app) {
         app.get("/positions", this::getPositions);
+        app.delete("/positions/{id}", this::deletePosition);
     }
 
     private void getPositions(Context ctx) {
         List<Position> positions = this.getPositionsUseCase.execute();
 
         ctx.json(positions);
+    }
+
+    private void deletePosition(Context ctx) {
+        String id = ctx.pathParam("id");
+
+        this.deletePositionUseCase.execute(id);
+
+        ctx.status(204);
     }
 }
